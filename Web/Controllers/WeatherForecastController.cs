c339441c7c693ef2cf406ce161a2e5eeea41adf3
Owen <owen.mcgregor@gmail.com>
2023-01-29 01:53:51 +0000

    test

--- Web/Controllers/WeatherForecastController.cs
+++ Web/Controllers/WeatherForecastController.cs
@@ -12,7 +12,7 @@ namespace VueUdemy.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "cold"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
